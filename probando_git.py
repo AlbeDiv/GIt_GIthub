51b11187be938d86c50312e2fa49c5d5e6261da5
AlbeDiv <alkirang30@gmail.com>
2025-06-08 20:31:49 -0500

    probando#

--- probando_git.py
+++ probando_git.py
@@ -3,5 +3,13 @@ print("Hola mundo")
 nombre = input("Ingrese su nombre por favor: ")
 print("Su nombre es: ", nombre)
 
-edad = input("Ingrese su edad por favor: ")
+edad = int(input("Ingrese su edad por favor: "))
 print("su edad es: ", edad)
+
+Materia1 = 4.0
+Materia2 = 3.0
+Materia3 = 4.5
+
+calificacion = (Materia1 + Materia2 + Materia3) / 3
+
+print("SU calificacion es: ", calificacion)
