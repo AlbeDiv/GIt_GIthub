a361913b6d49fd5b1b117beb7c9f6cd01a693e10
AlbeDiv <alkirang30@gmail.com>
2025-06-17 17:20:08 -0500

    Agregando ciudad

--- cambio.py
+++ cambio.py
@@ -2,3 +2,6 @@ print("Aprendiendo de git y control de versiones")
 
 apellido = input("Ingrese su apellido por favor: ")
 print("Su nombre es :", apellido)
+
+Ciudad = input("Ingrese la ciudad donde vive: ")
+print("la ciudad es: ")
